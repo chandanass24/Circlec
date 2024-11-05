5c166d20ab7f8024c8f2fc63dd6d0c5381eb5d43
ChandanaR-06 <chandana.an06@gmail.com>
2024-11-05 15:50:07 +0530

    Create area.c
    Area added

---
+++ area.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define PI 3.14159
+
+double area(double radius);
+double circumference(double radius);
+
+int main() {
+    double radius, circleArea, circleCircumference;
+
+
+    printf("Enter the radius of the circle: ");
+    scanf("%lf", &radius);
+
+
+    circleArea = area(radius);
+    circleCircumference = circumference(radius);
+
+    printf("Area of the circle: %.2lf\n", circleArea);
+    printf("Circumference of the circle: %.2lf\n", circleCircumference);
+
+    return 0;
+}
+double area(double r)
+{
+    return PI*r*r;
+}
